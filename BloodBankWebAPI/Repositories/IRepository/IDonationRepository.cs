9e9d7e981f1ca575666ecc2e260dbcbb7f7d1fe3
Rajshree Sonar <rajshreesonar0508@gmail.com>
2024-02-26 08:12:05 +0530

    implemented donation pdf

--- BloodBankWebAPI/Repositories/IRepository/IDonationRepository.cs
+++ BloodBankWebAPI/Repositories/IRepository/IDonationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using BloodBankWebAPI.Dtos.AddDtos;
 using BloodBankWebAPI.Dtos.GetDtos;
 using BloodBankWebAPI.Dtos.UpdateDtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BloodBankWebAPI.Repositories.IRepository
 {
@@ -8,7 +9,7 @@ namespace BloodBankWebAPI.Repositories.IRepository
     {
         void AddDonation(AddDonationDto donation);
         void UpdateDonation(UpdateDonationDto donation);
-
         IEnumerable<GetDonationDto> GetDonations();
+      //  byte[] GetDonationPdf();
     }
 }
